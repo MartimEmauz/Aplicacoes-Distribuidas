0d8afb9d5e56bcfa62fa7a3b6f60a6df5c13acf4
aluno-di <martim.emauz@gmail.com>
2023-02-22 22:10:40 +0000

    inicio

---
+++ ticker_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Aplicações Distribuídas - Projeto 1 - ticker_client.py
+Grupo: 27
+Números de aluno: 58668 | 58621
+"""
+# Zona para fazer imports
+
+
+
+# Programa principal
+
