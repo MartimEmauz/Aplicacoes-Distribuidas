6b7d02cc5db7b59df47b9565ea73b424dcacab9c
PauloBrandao <paulo.brandao@visualnuts.com>
2023-03-14 14:39:48 +0000

    .

--- projeto2/ticker_client.py
+++ projeto2/ticker_client.py
@@ -10,9 +10,7 @@ Números de aluno: 58668 | 58621
 import sock_utils as utils
 import socket as s
 import sys
-from net_client import server_connection
-import time
-import pickle
+from ticker_stub import TickerStub as Ticker
 
 # Programa principal
 
@@ -21,104 +19,14 @@ client_id = sys.argv[1]
 HOST = sys.argv[2]
 PORT = int(sys.argv[3])
 
-comandos = {
-    'SUBSCR': 10,
-    'CANCEL': 20,
-    'STATUS': 30,
-    'INFOS M': 40,
-    'INFOS K': 50,
-    'STATIS L': 60,
-    'STATIS ALL': 70,
-}
+stub = Ticker(HOST, PORT, client_id)
+
+stub.connect()
 
 while True:
-
+    
     comando = str(input("Comando > "))
     print(comando)
     comando = comando.split(" ")
+    stub.comando(comando)
 
-    if comando[0] == 'SUBSCR':
-        if  len(comando) <= 2:
-            print("MISSING-ARGUMENTS")
-
-        else:
-            connection = server_connection(HOST, PORT)
-            connection.connect()
-            comando[0] = comandos[comando[0]]
-            comando.append(client_id)
-            connection.send_receive(pickle.dumps([int(x) for x in comando], -1))
-            connection.close()
-
-    elif comando[0] == 'CANCEL':
-        if  len(comando) < 2:
-            print("MISSING-ARGUMENTS")
-
-        else:
-            connection = server_connection(HOST, PORT)
-            connection.connect()
-            comando[0] = comandos[comando[0]]
-            comando.append(client_id)
-            resposta = connection.send_receive(pickle.dumps([int(x) for x in comando], -1))
-            connection.close()
-
-    elif comando[0] == 'STATUS':
-        if  len(comando) < 2:
-            print("MISSING-ARGUMENTS")
-
-        else:
-            connection = server_connection(HOST, PORT)
-            connection.connect()
-            comando[0] = comandos[comando[0]]
-            comando.append(client_id)
-            resposta = connection.send_receive(pickle.dumps([int(x) for x in comando], -1))
-            connection.close()
-
-    elif comando[0] == 'INFOS' and comando[1] == 'M':
-        if  len(comando) < 2:
-            print("MISSING-ARGUMENTS")
-
-        connection = server_connection(HOST, PORT)
-        connection.connect()
-        comando = [comandos[" ".join(comando[0:2])]]
-        comando.append(client_id)
-        resposta = connection.send_receive(pickle.dumps([int(x) for x in comando], -1))
-        connection.close()
-
-    elif comando[0] == 'INFOS' and comando[1] == 'K':
-        if  len(comando) < 2:
-            print("MISSING-ARGUMENTS")
-
-        connection = server_connection(HOST, PORT)
-        connection.connect()
-        comando = [comandos[" ".join(comando[0:2])]]
-        comando.append(client_id)
-        resposta = connection.send_receive(pickle.dumps([int(x) for x in comando], -1))
-        connection.close()
-
-    elif comando[0] == 'STATIS' and comando[1] == 'L':
-        if  len(comando) <= 2:
-            print("MISSING-ARGUMENTS")
-        else:
-            connection = server_connection(HOST, PORT)
-            connection.connect()
-            newComando = [comandos[" ".join(comando[0:2])]]
-            newComando.append(comando[2])
-            resposta = connection.send_receive(pickle.dumps([int(x) for x in newComando], -1))
-            connection.close()
-
-    elif comando[0] == 'STATIS' and comando[1] == 'ALL':
-        if  len(comando) < 2:
-            print("MISSING-ARGUMENTS")
-        connection = server_connection(HOST, PORT)
-        connection.connect()
-        comando = [comandos[" ".join(comando[0:2])]]
-        resposta = connection.send_receive(pickle.dumps([int(x) for x in comando], -1))
-        connection.close()
-    elif comando[0] == 'SLEEP':
-        time.sleep(int(comando[1]))
-
-    elif comando[0] == 'EXIT':
-        break
-
-    else:
-        print("UNKNOWN-COMMAND")
---
+++ projeto2/ticker_stub.py
@@ -0,0 +1,107 @@
+import time
+from net_client import server_connection
+import pickle
+
+
+class TickerStub:
+    def __init__(self, host, port, client_id):
+        self.conn_sock = server_connection(host, port)
+        self.comandos = {
+            'SUBSCR': 10,
+            'CANCEL': 20,
+            'STATUS': 30,
+            'INFOS M': 40,
+            'INFOS K': 50,
+            'STATIS L': 60,
+            'STATIS ALL': 70,
+        }
+        self.client_id = client_id
+
+    def connect(self):
+        self.conn_sock.connect()
+
+    def disconnect(self):
+        # Fecha a ligação conn_sock
+        # Métodos tradicionais de um objeto do tipo lista
+        self.conn_sock.close()
+
+    def comando(self, comando):
+        # Envia o comando para o servidor
+        # Retorna a resposta do servidor
+        connection = self.conn_sock
+        if comando[0] == 'SUBSCR':
+            if len(comando) <= 2:
+                return "MISSING-ARGUMENTS"
+
+            else:
+                comando[0] = self.comandos[comando[0]]
+                comando.append(self.client_id)
+                connection.send_receive(pickle.dumps(
+                    [int(x) for x in comando], -1))
+
+        elif comando[0] == 'CANCEL':
+            if len(comando) < 2:
+                return "MISSING-ARGUMENTS"
+
+            else:
+
+                comando[0] = self.comandos[comando[0]]
+                comando.append(self.client_id)
+                resposta = connection.send_receive(
+                    pickle.dumps([int(x) for x in comando], -1))
+
+        elif comando[0] == 'STATUS':
+            if len(comando) < 2:
+                return "MISSING-ARGUMENTS"
+
+            else:
+
+                comando[0] = self.comandos[comando[0]]
+                comando.append(self.client_id)
+                resposta = connection.send_receive(
+                    pickle.dumps([int(x) for x in comando], -1))
+
+        elif comando[0] == 'INFOS' and comando[1] == 'M':
+            if len(comando) < 2:
+                return "MISSING-ARGUMENTS"
+
+            comando = [self.comandos[" ".join(comando[0:2])]]
+            comando.append(self.client_id)
+            resposta = connection.send_receive(
+                pickle.dumps([int(x) for x in comando], -1))
+
+        elif comando[0] == 'INFOS' and comando[1] == 'K':
+            if len(comando) < 2:
+                return "MISSING-ARGUMENTS"
+
+            comando = [self.comandos[" ".join(comando[0:2])]]
+            comando.append(self.client_id)
+            resposta = connection.send_receive(
+                pickle.dumps([int(x) for x in comando], -1))
+
+        elif comando[0] == 'STATIS' and comando[1] == 'L':
+            if len(comando) <= 2:
+                return "MISSING-ARGUMENTS"
+            else:
+
+                newComando = [self.comandos[" ".join(comando[0:2])]]
+                newComando.append(comando[2])
+                resposta = connection.send_receive(
+                    pickle.dumps([int(x) for x in newComando], -1))
+
+        elif comando[0] == 'STATIS' and comando[1] == 'ALL':
+            if len(comando) < 2:
+                return "MISSING-ARGUMENTS"
+
+            comando = [self.comandos[" ".join(comando[0:2])]]
+            resposta = connection.send_receive(
+                pickle.dumps([int(x) for x in comando], -1))
+        elif comando[0] == 'SLEEP':
+            time.sleep(int(comando[1]))
+
+        elif comando[0] == 'EXIT':
+            self.disconnect()
+            return "EXIT"
+
+        else:
+            return "UNKNOWN-COMMAND"
